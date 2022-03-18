f9a5dc7d4d268c558ee41e26a5afe5baaf9df410
Anastasya579 <101715415+Anastasya579@users.noreply.github.com>
2022-03-18 20:41:45 +0300

    Update Contacts.php

--- admin_dz_m1/admin/Contacts.php
+++ admin_dz_m1/admin/Contacts.php
@@ -3,12 +3,12 @@
 include "../autoload.php"; 
 include "../src/Model/Contact.php"; 
 
-
+use App\DB;
 use Model\Contact;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $contact = new Contact($_POST['name'], $_POST['mail'], $_POST['mob']);
-    $contact->save();
+$contact = new Contact($_POST['namet'], $_POST['mail'], $_POST['mob']);
+$contact->save();
 }
 echo '<pre>';
 print_r(Contact::getAll());
@@ -20,28 +20,27 @@ echo '</pre>';
   <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Form</title>
+            <title>Contact</title>
             <link rel="stylesheet" href="style.css">
   </head>
   <body>
-            <form action="Base.php" class="back-alert" method="post">
+            <form class="back-alert" method="POST">
             <h1>Форма заявки</h1>
             <div class="form-alert"> 
           <div>
               <label>Полное ФИО</label>
-              <input type="text" id="name" name="username" required><span>(обязательно для ввода)</span>
+              <input type="text" id="name" name="namet" required><span>(обязательно для ввода)</span>
             </div>
           <div>
               <label>Адрес электронной почты</label>
-              <input type="text" id="mail" name="usermail" required><span>(обязательно для ввода)</span>
+              <input type="text" id="mail" name="mail" required><span>(обязательно для ввода)</span>
             </div>
           <div>
               <label>Номер телефона</label>
-              <input type="text" id="mob" name="numbermob" required><span>(обязательно для ввода)</span>
+              <input type="text" id="mob" name="mob" required><span>(обязательно для ввода)</span>
           </div>
               <input class="bot-send-mail" type="submit" value="Отправить заявку">
               <input class="bot-send-mail" type="reset" value="Очистить форму">
         </div></form>
   </body>
   </html>
-
